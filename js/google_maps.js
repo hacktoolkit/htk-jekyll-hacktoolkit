ac3b1500f8acfabeb64a54eb44f26aaf8ab52107
Jonathan Tsai <jontsai@jonathantsai.com>
2015-03-16 05:32:48 +0000

    added htk.js and related refactoring

--- js/google_maps.js
+++
@@ -1,9 +0,0 @@
-function initialize() {
-    var mapOptions = {
-        center: new google.maps.LatLng(-34.397, 150.644),
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
